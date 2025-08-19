75c44700f4377acd7cace92fdba20da2f6cc8c3e
Guilherme Sartori <gsartori-ext@techforti.com.br>
2025-08-19 16:47:03 -0300

    add public port in listen

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   // Configurar prefixo global para API
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 void bootstrap();
